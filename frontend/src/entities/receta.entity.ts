d3859f64e101bc8857c367c235e9ad30a97529f2
Martina <martinapanto2006@gmail.com>
2026-08-01 16:02:22 -0300

    actualizacion receta.entity

--- frontend/src/entities/receta.entity.ts
+++ frontend/src/entities/receta.entity.ts
@@ -3,12 +3,14 @@ import { Etiqueta } from "./etiqueta.entity";
 import { Paso } from "./paso.entity";
 import { Utensilio } from "./utensilio.entity";
 import { RecetaIngrediente } from "./receta_ingrediente.entity";
+import { Usuario } from "./usuario.entity"
 
 export class Receta extends baseEntity {
   nombre!: string;
   dificultad!: string;
   tiempoMin!: number;
   estado!: string;
+  usuarios?: Usuario;
   etiquetas?: Etiqueta[];
   pasos?: Paso[];
   utensilios?: Utensilio[];
