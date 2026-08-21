f88d55d299eff0df38828af21ece31c452ecc134
Candela Baras <candebaras@gmail.com>
2026-08-21 10:38:53 -0300

    Corrijo doble BrowserRouter anidado que rompia la app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/layout";
 import HomePage from "./pages/homePage";
 import EtiquetasPage from "./pages/etiquetaPage";
@@ -7,15 +7,13 @@ import "./index.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/etiquetas" element={<EtiquetasPage />} />
-          <Route path="/ingredientes" element={<IngredientesPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/etiquetas" element={<EtiquetasPage />} />
+        <Route path="/ingredientes" element={<IngredientesPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
