a6a8342fcd237f0bafd15c26c4c68ef15a3d6763
Luisina <lulimoldani@gmail.com>
2026-09-03 12:53:03 -0300

    Actualizo atributos de  restriccion_alimentaria

--- frontend/src/entities/restriccion_alimentaria.entity.ts
+++ frontend/src/entities/restriccion_alimentaria.entity.ts
@@ -1,7 +1,8 @@
+import { baseEntity } from "./baseEntity";
 import { TipoRestriccion } from "./tipo_restriccion.entity";
 
-export class RestriccionAlimentaria {
-  tipoRestriccion!: TipoRestriccion;
+export class RestriccionAlimentaria extends baseEntity {
   nombre!: string;
   descripcion?: string;
+  tipoRestriccion!: TipoRestriccion;
 }
