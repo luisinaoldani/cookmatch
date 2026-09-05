b55665ff54b26ad1ced663a54f5da49ed39e64af
Martina Rodriguez <marturodri04@gmail.com>
2026-09-05 13:55:53 -0300

    Conecto 'Utensilio' con la API real, saco mock

--- frontend/src/services/utensilio.service.ts
+++ frontend/src/services/utensilio.service.ts
@@ -1,35 +1,26 @@
+import api from "./axiosConfig";
 import { Utensilio } from "../entities/utensilio.entity";
-import { UtensilioMock } from "./mocks/utensilio.mock";
-
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
-let data = [...UtensilioMock];
-let nextId = data.length + 1;
 
 export const getUtensilios = async (): Promise<Utensilio[]> => {
-  await delay(400);
-  return data;
+  const res = await api.get("/utensilios");
+  return res.data;
 };
 
-export const getUtensilioById = async (id: number): Promise<Utensilio | undefined> => {
-  await delay(300);
-  return data.find((u) => u.id === id);
+export const getUtensilioById = async (id: number): Promise<Utensilio> => {
+  const res = await api.get(`/utensilios/${id}`);
+  return res.data;
 };
 
 export const createUtensilio = async (newItem: Partial<Utensilio>): Promise<Utensilio> => {
-  await delay(300);
-  const utensilio = { ...newItem, id: nextId++ } as Utensilio;
-  data.push(utensilio);
-  return utensilio;
+  const res = await api.post("/utensilios", newItem);
+  return res.data;
 };
 
-export const updateUtensilio = async (id: number, changes: Partial<Utensilio>): Promise<Utensilio | undefined> => {
-  await delay(300);
-  data = data.map((u) => (u.id === id ? { ...u, ...changes } : u));
-  return data.find((u) => u.id === id);
+export const updateUtensilio = async (id: number, changes: Partial<Utensilio>): Promise<Utensilio> => {
+  const res = await api.put(`/utensilios/${id}`, changes);
+  return res.data;
 };
 
 export const deleteUtensilio = async (id: number): Promise<void> => {
-  await delay(300);
-  data = data.filter((u) => u.id !== id);
+  await api.delete(`/utensilios/${id}`);
 };
